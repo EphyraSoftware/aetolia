8b83b54d3b16e345d8cb4dd63535a807a954dc50
ThetaSinner <ThetaSinner@users.noreply.github.com>
2024-05-09 00:53:20 +0100

    Parse ALTREP param

--- src/parser.rs
+++ src/parser.rs
@@ -1,12 +1,12 @@
-use nom::{AsChar, IResult, Parser};
 use nom::branch::alt;
 use nom::bytes::complete::{take_until, take_while1};
 use nom::bytes::streaming::tag;
-use nom::character::streaming::{alpha1, alphanumeric1, char, crlf};
-use nom::combinator::opt;
+use nom::character::streaming::{alphanumeric1, char, crlf};
+use nom::combinator::{opt, recognize};
 use nom::error::{ErrorKind, ParseError};
 use nom::multi::separated_list1;
 use nom::sequence::{separated_pair, tuple};
+use nom::{AsChar, IResult, Parser};
 
 #[derive(Clone, Debug, PartialEq)]
 pub struct Error<'a> {
@@ -18,6 +18,7 @@ pub struct Error<'a> {
 pub enum InnerError {
     Nom(ErrorKind),
     XNameTooShort,
+    EncodingError(String, std::str::Utf8Error),
 }
 
 impl<'a> Error<'a> {
@@ -51,63 +52,113 @@ impl<'a> From<(&'a [u8], ErrorKind)> for Error<'a> {
     }
 }
 
-struct ContentLine {
-    property_name: Vec<u8>,
+#[derive(Debug, Clone)]
+struct ContentLine<'a> {
+    property_name: &'a [u8],
     value: Vec<u8>,
 }
 
-fn iana_token(input: &[u8]) -> IResult<&[u8], &[u8], Error>
-{
+#[derive(Debug, Clone, Eq, PartialEq)]
+struct Param {
+    name: String,
+    value: ParamValue,
+}
+
+#[derive(Debug, Clone, Eq, PartialEq)]
+enum ParamValue {
+    AltRep { uri: String },
+}
+
+fn iana_token(input: &[u8]) -> IResult<&[u8], &[u8], Error> {
     take_while1(|c: u8| c.is_alphanum() || c == b'-')(input)
 }
 
-fn vendor_id(input: &[u8]) -> IResult<&[u8], &[u8], Error>
-{
+fn vendor_id(input: &[u8]) -> IResult<&[u8], &[u8], Error> {
     let (rest, id) = alphanumeric1(input)?;
 
     if id.len() < 3 {
-        return Err(nom::Err::Failure(Error::new(rest, InnerError::XNameTooShort)))
+        return Err(nom::Err::Failure(Error::new(
+            rest,
+            InnerError::XNameTooShort,
+        )));
     }
 
     Ok((rest, id))
 }
 
-fn x_name(input: &[u8]) -> IResult<&[u8], Vec<u8>, Error>
-{
-    let (input, (prefix, maybe_vendor_id, name)) = tuple((tag("X-"), opt(tuple((vendor_id, char('-')))), take_while1(|c: u8| c.is_alphanum() || c == b'-')))(input)?;
+fn x_name(input: &[u8]) -> IResult<&[u8], &[u8], Error> {
+    let (input, x_name) = recognize(tuple((
+        tag("X-"),
+        opt(tuple((vendor_id, char('-')))),
+        take_while1(|c: u8| c.is_alphanum() || c == b'-'),
+    )))(input)?;
 
-    let mut n = prefix.to_vec();
-    if let Some((vendor_id, _)) = maybe_vendor_id {
-        n.extend_from_slice(vendor_id);
-        n.push(b'-');
-    }
-    n.extend_from_slice(name);
-
-    Ok((input, n))
+    Ok((input, x_name))
 }
 
-fn name(input: &[u8]) -> IResult<&[u8], Vec<u8>, Error>
-{
-    alt((iana_token.map(|t| t.to_vec()), x_name))(input)
+fn name(input: &[u8]) -> IResult<&[u8], &[u8], Error> {
+    alt((iana_token, x_name))(input)
 }
 
-fn param_name(input: &[u8]) -> IResult<&[u8], Vec<u8>, Error>
-{
-    alt((iana_token.map(|t| t.to_vec()), x_name))(input)
+fn param_name(input: &[u8]) -> IResult<&[u8], &[u8], Error> {
+    alt((iana_token, x_name))(input)
 }
 
-fn param(input: &[u8]) -> IResult<&[u8], (Vec<u8>, Vec<u8>), Error>
-{
-    let (input, (name, _, value)) = tuple((param_name, char('='), take_until(";")))(input)?;
+fn param(input: &[u8]) -> IResult<&[u8], Option<Param>, Error> {
+    let (input, (name, _)) = tuple((param_name, char('=')))(input)?;
 
-    Ok((input, (name, value.to_vec())))
+    let name_s = read_string(name, "param_name")?;
+    let (input, maybe_param_value) = match name_s.as_str() {
+        "ALTREP" => {
+            let (input, (_, uri, _)) = tuple((char('"'), take_until("\""), char('"')))(input)?;
+
+            (
+                input,
+                Some(ParamValue::AltRep {
+                    uri: read_string(&uri, "uri")?,
+                }),
+            )
+        }
+        _ => {
+            // TODO not robust! Check 3
+            let (input, _) = take_until(";")(input)?;
+
+            (input, None)
+        }
+    };
+
+    Ok((
+        input,
+        if let Some(param_value) = maybe_param_value {
+            Some(Param {
+                name: name_s,
+                value: param_value,
+            })
+        } else {
+            None
+        },
+    ))
+}
+
+fn read_string<'a>(input: &'a[u8], context: &str) -> Result<String, nom::Err<Error<'a>>> {
+    Ok(std::str::from_utf8(input)
+        .map_err(|e| {
+            nom::Err::Failure(Error::new(
+                input,
+                InnerError::EncodingError(context.to_string(), e),
+            ))
+        })?
+        .to_string())
 }
 
 fn parse_line_content(input: &[u8]) -> IResult<&[u8], Vec<u8>, Error> {
-    let (input, (parts, _)) = tuple((separated_list1(
-        tuple((crlf, alt((char(' '), char('\t'))))),
-        take_until("\r\n"),
-    ), crlf))(input)?;
+    let (input, (parts, _)) = tuple((
+        separated_list1(
+            tuple((crlf, alt((char(' '), char('\t'))))),
+            take_until("\r\n"),
+        ),
+        crlf,
+    ))(input)?;
 
     Ok((
         input,
@@ -119,11 +170,8 @@ fn parse_line_content(input: &[u8]) -> IResult<&[u8], Vec<u8>, Error> {
 }
 
 fn parse_line(input: &[u8]) -> IResult<&[u8], ContentLine, Error> {
-    let (input, (property_name, value)) = separated_pair(
-        name,
-        char(':'),
-        parse_line_content,
-    )(input)?;
+    let (input, (property_name, value)) =
+        separated_pair(name, char(':'), parse_line_content)(input)?;
 
     Ok((
         input,
@@ -149,34 +197,58 @@ mod tests {
     fn simple_x_name() {
         let (rem, x_name) = x_name(b"X-TEST ").unwrap();
         check_rem(rem, 1);
-        assert_eq!(b"X-TEST", x_name.as_slice());
+        assert_eq!(b"X-TEST", x_name);
     }
 
     #[test]
     fn simple_x_name_with_vendor() {
         let (rem, x_name) = x_name(b"X-ESL-TEST ").unwrap();
         check_rem(rem, 1);
-        assert_eq!(b"X-ESL-TEST", x_name.as_slice());
+        assert_eq!(b"X-ESL-TEST", x_name);
+    }
+
+    #[test]
+    fn param_altrep() {
+        let (rem, param) = param(b"ALTREP=\"http://example.com/calendar\";").unwrap();
+        check_rem(rem, 1);
+        let param = param.unwrap();
+        assert_eq!("ALTREP", param.name);
+        assert_eq!(
+            ParamValue::AltRep {
+                uri: "http://example.com/calendar".to_string()
+            },
+            param.value
+        );
     }
 
     #[test]
     fn simple_content_line() {
-        let (rem, content_line) =
-            parse_line(b"DESCRIPTION:This is a long description that exists on a long line.\r\nnext")
-                .unwrap();
+        let (rem, content_line) = parse_line(
+            b"DESCRIPTION:This is a long description that exists on a long line.\r\nnext",
+        )
+        .unwrap();
         check_rem(rem, 4);
-        assert_eq!(b"DESCRIPTION", content_line.property_name.as_slice());
-        assert_eq!(b"This is a long description that exists on a long line.", content_line.value.as_slice());
+        assert_eq!(b"DESCRIPTION", content_line.property_name);
+        assert_eq!(
+            b"This is a long description that exists on a long line.",
+            content_line.value.as_slice()
+        );
     }
 
     #[test]
     fn content_line_multi_line() {
-        let (rem, content_line) =
-            parse_line(b"DESCRIPTION:This is a lo\r\n ng description\r\n  that exists on a long line.\r\nnext")
-                .unwrap();
+        let (rem, content_line) = parse_line(
+            b"DESCRIPTION:This is a lo\r\n ng description\r\n  that exists on a long line.\r\nnext",
+        )
+        .unwrap();
         check_rem(rem, 4);
-        assert_eq!(b"DESCRIPTION", content_line.property_name.as_slice());
-        assert_eq!(b"This is a long description that exists on a long line.", content_line.value.as_slice(), "Got: {}", String::from_utf8(content_line.value.clone()).unwrap());
+        assert_eq!(b"DESCRIPTION", content_line.property_name);
+        assert_eq!(
+            b"This is a long description that exists on a long line.",
+            content_line.value.as_slice(),
+            "Got: {}",
+            String::from_utf8(content_line.value.clone()).unwrap()
+        );
     }
 
     #[test]
@@ -185,8 +257,13 @@ mod tests {
             parse_line(b"DESCRIPTION:This is a lo\r\n ng description\r\n\t that exists on a long line.\r\nnext")
                 .unwrap();
         check_rem(rem, 4);
-        assert_eq!(b"DESCRIPTION", content_line.property_name.as_slice());
-        assert_eq!(b"This is a long description that exists on a long line.", content_line.value.as_slice(), "Got: {}", String::from_utf8(content_line.value.clone()).unwrap());
+        assert_eq!(b"DESCRIPTION", content_line.property_name);
+        assert_eq!(
+            b"This is a long description that exists on a long line.",
+            content_line.value.as_slice(),
+            "Got: {}",
+            String::from_utf8(content_line.value.clone()).unwrap()
+        );
     }
 
     fn check_rem(rem: &[u8], expected_len: usize) {
@@ -194,6 +271,11 @@ mod tests {
             let str = String::from_utf8(rem.to_vec()).unwrap();
             println!("rem: {str}");
         }
-        assert_eq!(expected_len, rem.len(), "Remainder length should be {expected_len} but was {}", rem.len());
+        assert_eq!(
+            expected_len,
+            rem.len(),
+            "Remainder length should be {expected_len} but was {}",
+            rem.len()
+        );
     }
 }
