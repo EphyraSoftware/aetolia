a478f1f0382995c389b59689d3f957e52aadfc45
ThetaSinner <greatdjonfire@hotmail.co.uk>
2024-07-11 02:37:05 +0100

    Validate encoding

--- src/validate.rs
+++ src/validate.rs
@@ -1,11 +1,14 @@
 mod error;
 
+use anyhow::Context;
 use crate::model::{CalendarComponent, CalendarProperty, ComponentProperty, ICalObject, Param};
 use crate::validate::error::{CalendarPropertyError, ICalendarError, ParamError};
 
 pub use error::*;
+use crate::common::{Encoding, Value};
+use crate::serialize::WriteModel;
 
-pub fn validate_model(ical_object: ICalObject) -> Vec<ICalendarError> {
+pub fn validate_model(ical_object: ICalObject) -> anyhow::Result<Vec<ICalendarError>> {
     let mut errors = Vec::new();
 
     errors.extend_from_slice(
@@ -20,7 +23,7 @@ pub fn validate_model(ical_object: ICalObject) -> Vec<ICalendarError> {
             CalendarComponent::Event(event) => {
                 errors.extend_from_slice(
                     ICalendarError::many_from_component_property_errors(
-                        validate_component_properties(&event.properties),
+                        validate_component_properties(&event.properties)?,
                         index,
                         component_name(component).to_string(),
                     )
@@ -30,7 +33,7 @@ pub fn validate_model(ical_object: ICalObject) -> Vec<ICalendarError> {
             CalendarComponent::TimeZone(time_zone) => {
                 errors.extend_from_slice(
                     ICalendarError::many_from_component_property_errors(
-                        validate_component_properties(&time_zone.properties),
+                        validate_component_properties(&time_zone.properties)?,
                         index,
                         component_name(component).to_string(),
                     )
@@ -40,7 +43,7 @@ pub fn validate_model(ical_object: ICalObject) -> Vec<ICalendarError> {
             CalendarComponent::XComponent(x_component) => {
                 errors.extend_from_slice(
                     ICalendarError::many_from_component_property_errors(
-                        validate_component_properties(&x_component.properties),
+                        validate_component_properties(&x_component.properties)?,
                         index,
                         component_name(component).to_string(),
                     )
@@ -53,13 +56,15 @@ pub fn validate_model(ical_object: ICalObject) -> Vec<ICalendarError> {
         }
     }
 
-    errors
+    Ok(errors)
 }
 
-fn validate_component_properties(properties: &[ComponentProperty]) -> Vec<ComponentPropertyError> {
+fn validate_component_properties(properties: &[ComponentProperty]) -> anyhow::Result<Vec<ComponentPropertyError>> {
     let mut errors = Vec::new();
 
     for (index, property) in properties.iter().enumerate() {
+        check_encoding_for_binary_values(&mut errors, property, index)?;
+
         match property {
             ComponentProperty::Description(description) => {
                 let property_info = PropertyInfo::new(ValueType::Text);
@@ -81,12 +86,64 @@ fn validate_component_properties(properties: &[ComponentProperty]) -> Vec<Compon
         }
     }
 
-    errors
+    Ok(errors)
+}
+
+fn check_encoding_for_binary_values(errors: &mut Vec<ComponentPropertyError>, property: &ComponentProperty, property_index: usize) -> anyhow::Result<()> {
+    let declared_value_type = get_declared_value_type(property);
+
+    if let Some((value_type, value_type_index)) = declared_value_type {
+        if value_type == Value::Binary {
+            let mut encoding_param_found = false;
+            for param in property.params() {
+                if let Param::Encoding { encoding } = param {
+                    encoding_param_found = true;
+
+                    if *encoding != Encoding::Base64 {
+                        let mut msg = b"Property is declared to have a binary value but the encoding is set to ".to_vec();
+                        encoding.write_model(&mut msg).context("Failed to write encoding to model")?;
+                        msg.extend_from_slice(", instead of BASE64".as_bytes());
+
+                        errors.push(ComponentPropertyError {
+                            message: String::from_utf8_lossy(&msg).to_string(),
+                            location: Some(ComponentPropertyLocation {
+                                index: property_index,
+                                name: component_property_name(property).to_string(),
+                                property_location: Some(WithinPropertyLocation::Param {
+                                    index: value_type_index,
+                                    name: "VALUE".to_string(),
+                                }),
+                            }),
+                        });
+                    }
+                }
+            }
+
+            if !encoding_param_found {
+                errors.push(ComponentPropertyError {
+                    message: "Property is declared to have a binary value but no encoding is set, must be set to BASE64".to_string(),
+                    location: Some(ComponentPropertyLocation {
+                        index: property_index,
+                        name: component_property_name(property).to_string(),
+                        property_location: Some(WithinPropertyLocation::Param {
+                            index: value_type_index,
+                            name: "VALUE".to_string(),
+                        }),
+                    }),
+                });
+            }
+        }
+    }
+
+    Ok(())
 }
 
 #[derive(Debug)]
 struct PropertyInfo {
     value_type: ValueType,
+    /// If the property has a VALUE parameter, regardless of whether that is valid on this
+    /// property, then it will be populated here.
+    declared_value_type: Option<crate::common::Value>,
     /// This is an xProperty or ianaProperty
     is_other: bool,
 }
@@ -95,6 +152,7 @@ impl PropertyInfo {
     fn new(value_type: ValueType) -> Self {
         PropertyInfo {
             value_type,
+            declared_value_type: None,
             is_other: false,
         }
     }
@@ -172,6 +230,21 @@ fn validate_params(params: &[Param], property_info: PropertyInfo) -> Vec<ParamEr
             }
             Param::Other { name, .. } | Param::Others { name, .. } if name == "DIR" => {
                 validate_dir_param(&mut errors, param, index, &property_info);
+            }
+            Param::ValueType { .. } => {
+                // Nothing to validate yet
+            }
+            Param::Encoding { .. } => {
+                // Nothing further to validate
+            }
+            Param::FormatType { .. } => {
+                // Format type is not validated by this program
+            }
+            Param::FreeBusyTimeType { .. } => {
+                // Nothing further to validate
+            }
+            Param::Language { .. } => {
+                // Nothing further to validate
             }
             _ => {
                 unimplemented!()
@@ -266,10 +339,10 @@ fn validate_dir_param(
     }
 }
 
-fn get_declared_value_type(property: &ComponentProperty) -> Option<crate::common::Value> {
-    property.params().iter().find_map(|param| {
+fn get_declared_value_type(property: &ComponentProperty) -> Option<(Value, usize)> {
+    property.params().iter().enumerate().find_map(|(index, param)| {
         if let Param::ValueType { value } = param {
-            return Some(value.clone());
+            return Some((value.clone(), index));
         }
 
         None
@@ -501,10 +574,38 @@ END:VCALENDAR\r\n";
         assert_eq!("In component \"VEVENT\" at index 0, in component property \"DESCRIPTION\" at index 0: Directory entry reference (DIR) is not allowed for this property type", errors.first().unwrap().to_string());
     }
 
+    #[test]
+    fn encoding_not_set_on_binary_value() {
+        let content = "BEGIN:VCALENDAR\r\n\
+BEGIN:VEVENT\r\n\
+DESCRIPTION;VALUE=BINARY:eA==\r\n\
+END:VEVENT\r\n\
+END:VCALENDAR\r\n";
+
+        let errors = validate_content(content);
+
+        assert_eq!(errors.len(), 1);
+        assert_eq!("In component \"VEVENT\" at index 0, in component property \"DESCRIPTION\" at index 0: Property is declared to have a binary value but no encoding is set, must be set to BASE64", errors.first().unwrap().to_string());
+    }
+
+    #[test]
+    fn encoding_set_to_8bit_on_binary_value() {
+        let content = "BEGIN:VCALENDAR\r\n\
+BEGIN:VEVENT\r\n\
+DESCRIPTION;VALUE=BINARY;ENCODING=8BIT:eA==\r\n\
+END:VEVENT\r\n\
+END:VCALENDAR\r\n";
+
+        let errors = validate_content(content);
+
+        assert_eq!(errors.len(), 1);
+        assert_eq!("In component \"VEVENT\" at index 0, in component property \"DESCRIPTION\" at index 0: Property is declared to have a binary value but the encoding is set to 8BIT, instead of BASE64", errors.first().unwrap().to_string());
+    }
+
     fn validate_content(content: &str) -> Vec<ICalendarError> {
         let (rem, object) = crate::parser::ical_object::<Error>(content.as_bytes()).unwrap();
         check_rem(rem, 0);
 
-        validate_model(object.to_model().unwrap())
+        validate_model(object.to_model().unwrap()).unwrap()
     }
 }
