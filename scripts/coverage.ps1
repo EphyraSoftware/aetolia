4b1bf98ebdd654d357034b18ed9c0c9389c76c2b
ThetaSinner <greatdjonfire@hotmail.co.uk>
2024-07-29 02:55:38 +0100

    Script getting coverage

---
+++ scripts/coverage.ps1
@@ -0,0 +1,6 @@
+$env:RUSTFLAGS="-Cinstrument-coverage"
+$env:LLVM_PROFILE_FILE="aetolia-%p-%m.profraw"
+
+(Get-ChildItem -Path $Path).Fullname -match ".*.profraw" | Remove-Item
+cargo test
+grcov . -s . --binary-path ./target/debug/ -t html --branch --ignore-not-existing -o ./target/debug/coverage/
