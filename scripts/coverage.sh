ee2d51c9c92cc401ae040fa5aa56d717b3e92b9c
ThetaSinner <greatdjonfire@hotmail.co.uk>
2024-08-08 17:09:24 +0100

    Add script for coverage on Linux

---
+++ scripts/coverage.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+cargo clean
+cargo build
+
+export RUSTFLAGS="-Cinstrument-coverage"
+export LLVM_PROFILE_FILE="aetolia-%p-%m.profraw"
+
+find --print0 . -iname "*.profraw" | xargs rm
+cargo test
+grcov . -s . --binary-path ./target/debug/ -t html --branch --ignore-not-existing -o ./target/debug/coverage/
