1011b3627505263c272428b1ebba7c0cb7fada9a
ThetaSinner <greatdjonfire@hotmail.co.uk>
2024-08-08 18:30:53 +0100

    Fix coverage script

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -8,6 +8,6 @@ cargo build
 export RUSTFLAGS="-Cinstrument-coverage"
 export LLVM_PROFILE_FILE="aetolia-%p-%m.profraw"
 
-find --print0 . -iname "*.profraw" | xargs rm
+rm "*.profraw"
 cargo test
 grcov . -s . --binary-path ./target/debug/ -t html --branch --ignore-not-existing -o ./target/debug/coverage/
