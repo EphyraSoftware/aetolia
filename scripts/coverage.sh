d8b8b866c0a4c5fd5760cfac71118bdeb88f1ea6
ThetaSinner <greatdjonfire@hotmail.co.uk>
2024-08-10 03:36:51 +0100

    Allow no matches

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -8,7 +8,7 @@ cargo build
 export RUSTFLAGS="-Cinstrument-coverage"
 export LLVM_PROFILE_FILE="aetolia-%p-%m.profraw"
 
-rm "*.profraw"
+rm "*.profraw" || true
 cargo test
 cargo run --example load_sample
 grcov . -s . --binary-path ./target/debug/ -t html --branch --ignore-not-existing -o ./target/debug/coverage/
